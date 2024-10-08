2d07f0cabbcd266ff4dd19a496dc6774dd8af8a5
kerogs <luckywies@gmail.com>
2024-10-08 22:15:18 +0200

    add test code

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-m
+import markdown
+
+print("Start...")
+
+md_text = """
+# Titre
+Ce texte est **en gras** et _en italique_.
+```php
+hello world!!
+```
+``/path/example/``
+## Subtitle
+### Monkey wizardGang
+> Hello world
+> I am an important message haha
+
+~~remove me~~
+[link](http://www.google.com)
+![img](dzpofkezoip)
+okay letsgo !!!!!!
+
+===oublier ce que sa fait.===
+"""
+
+html = markdown.markdown(md_text)
+
+print(html)
